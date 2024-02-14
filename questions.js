91df3e8199cc34870a2bcc520763ea286357b223
Shakeelwani <153032223+Shakeelwani@users.noreply.github.com>
2024-02-14 13:09:54 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,62 @@
+let questions = [
+    {
+        numb:1,
+        question: "what does HTML stand for?",
+        answer: "C. hyper text markup language",
+        options:[
+            "A. hyper type multi language",
+            "B. hyper text multiple language",
+            "C. hyper text markup language",
+            "D. home text multi language"
+
+        ]
+    },
+    {
+        numb:2,
+        question: "what does css stand for",
+        answer: "A. cascading style sheet",
+        options:[
+            "A. cascading style sheet",
+            "B. computer style sheet",
+            "C. cute style sheet",
+            "D. corse style shorts",
+
+        ]
+    },
+    {
+        numb:3,
+        question: "what does PHP stands for?",
+        answer: "A. hypertext preprocessor",
+        options:[
+            "A. hypertext preprocessor",
+            "B. hometext preprocees",
+            "C. hello proocess",
+            "D. programming hypertext preprocessor"
+
+        ]
+    },
+    {
+        numb:4,
+        question: "what does SQL stands for?",
+        answer: "D.structured query language",
+        options:[
+            "A. store query language",
+            "B. shak query language",
+            "C. simple query language",
+            "D. structured query language"
+
+        ]
+    },
+    {
+        numb:5,
+        question: "what does XML stands for?",
+        answer: "D. extensible multiple lamguage ",
+        options:[
+            "A. extra multiply language",
+            "B. excellent mold languge",
+            "C. explorer mold language",
+            "D. extensible multiple lamguage"
+
+        ]
+    },
+]
